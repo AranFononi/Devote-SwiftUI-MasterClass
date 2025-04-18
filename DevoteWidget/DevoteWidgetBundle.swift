e17d5e3a4228626e53b976a43806d2afea11e9b9
Aran Fononi <aaronfononi@gmail.com>
2025-04-18 14:01:22 +0330

    Create Widget and add assets to it

---
+++ DevoteWidget/DevoteWidgetBundle.swift
@@ -0,0 +1,17 @@
+//
+//  DevoteWidgetBundle.swift
+//  DevoteWidget
+//
+//  Created by Aran Fononi on 18/4/25.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct DevoteWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        DevoteWidget()
+        DevoteWidgetLiveActivity()
+    }
+}
